f6f0b5647623e41ca2d36929dbe7e5dafe018e97
filiplyrheden <filip.lyrheden@gmail.com>
2024-09-30 18:23:31 +0200

    added table.php, data.php and index.php

---
+++ table.php
@@ -0,0 +1,46 @@
+<table>
+    <tr>
+        <th>Team</th>
+        <th>Uefa Ranking</th>
+    </tr>
+    <tr>
+        <td>Barcelona</td>
+        <td>1</td>
+    </tr>
+    <tr>
+        <td>Paris Saint-Germain</td>
+        <td>2</td>
+    </tr>
+    <tr>
+        <td>Lyon</td>
+        <td>3</td>
+    </tr>
+    <tr>
+        <td>Wolfsburg</td>
+        <td>4</td>
+    </tr>
+    <tr>
+        <td>Bayern Munich</td>
+        <td>5</td>
+    </tr>
+    <tr>
+        <td>Chelsea</td>
+        <td>6</td>
+    </tr>
+    <tr>
+        <td>Arsenal</td>
+        <td>7</td>
+    </tr>
+    <tr>
+        <td>Real Madrid</td>
+        <td>8</td>
+    </tr>
+    <tr>
+        <td>Manchester City</td>
+        <td>9</td>
+    </tr>
+    <tr>
+        <td>Juventus</td>
+        <td>10</td>
+    </tr>
+</table>
