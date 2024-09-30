e0a7fb58538fb04334217913857e494c30c6ceed
filiplyrheden <filip.lyrheden@gmail.com>
2024-09-30 19:03:30 +0200

    added logic.php and updated table.php with foreach-loop

---
+++ logic.php
@@ -0,0 +1,5 @@
+<?php
+
+require __DIR__ . "/data.php";
+
+foreach ($teams as $teamName => $teamData);
--- table.php
+++ table.php
@@ -1,59 +1,30 @@
 <?php
 
 require __DIR__ . "/head.php";
+require __DIR__ . "/logic.php";
 
 ?>
 
 <table>
+
     <tr>
         <th>Team</th>
         <th>Logo</th>
         <th>Uefa Ranking</th>
     </tr>
-    <tr>
-        <td>
-            <h3>Barcelona</h3>
-            <p><b>City:</b>Barcelona</p>
-            <p><b>Group:</b>D</p>
-            <p><a href="https://www.fcbarcelona.com/">Website</a></p>
-        </td>
-        <td><img src="https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg" /></td>
-        <td>1</td>
-    </tr>
-    <tr>
-        <td>Paris Saint-Germain</td>
-        <td>2</td>
-    </tr>
-    <tr>
-        <td>Lyon</td>
-        <td>3</td>
-    </tr>
-    <tr>
-        <td>Wolfsburg</td>
-        <td>4</td>
-    </tr>
-    <tr>
-        <td>Bayern Munich</td>
-        <td>5</td>
-    </tr>
-    <tr>
-        <td>Chelsea</td>
-        <td>6</td>
-    </tr>
-    <tr>
-        <td>Arsenal</td>
-        <td>7</td>
-    </tr>
-    <tr>
-        <td>Real Madrid</td>
-        <td>8</td>
-    </tr>
-    <tr>
-        <td>Manchester City</td>
-        <td>9</td>
-    </tr>
-    <tr>
-        <td>Juventus</td>
-        <td>10</td>
-    </tr>
+
+    <?php foreach ($teams as $teamName => $teamData) : ?>
+        <tr>
+            <td>
+                <h3><?= $teamName ?></h3>
+                <p><b>City:</b> <?= $teamData["city"] ?></p>
+                <p><b>League:</b> <?= $teamData["league"] ?></p>
+                <p><b>Group:</b> <?= $teamData["group"] ?></p>
+                <p><a href="<?= $teamData["url"] ?>" target="_blank">Website</a></p>
+            </td>
+            <td><img src="<?= $teamData["logo"] ?>" alt="<?= $teamName ?> Logo" width="100"></td>
+            <td><?= $teamData["uefa-coefficient-ranking"] ?></td>
+        </tr>
+    <?php endforeach; ?>
+
 </table>
