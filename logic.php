f76c1e3327cbecd457fc08eaf4c14e085ec1ed63
filiplyrheden <filip.lyrheden@gmail.com>
2024-10-02 11:07:47 +0200

    added functions to compare and sort teams by uefa ranking

--- logic.php
+++ logic.php
@@ -2,4 +2,14 @@
 
 require __DIR__ . "/data.php";
 
-foreach ($teams as $teamName => $teamData);
+//Function that compares uefa ranking and ranks teams without ranking last.
+function compare_uefa_ranking($a, $b)
+{
+    $rankA = is_numeric($a['uefa-coefficient-ranking']) ? $a['uefa-coefficient-ranking'] : PHP_INT_MAX;
+    $rankB = is_numeric($b['uefa-coefficient-ranking']) ? $b['uefa-coefficient-ranking'] : PHP_INT_MAX;
+
+    return $rankA <=> $rankB;
+}
+
+//Sorts teams by uefa ranking
+uasort($teams, 'compare_uefa_ranking');
