2a7e44119173831caa5ddb3385044e3bf8edea84
filiplyrheden <filip.lyrheden@gmail.com>
2024-10-02 14:40:51 +0200

    Fixed broken links in nav

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <body>
     <header>
         <nav>
-            <a href="/index.php">Start</a>
-            <a href="/about.php">About</a>
+            <a href="index.php">Start</a>
+            <a href="about.php">About</a>
         </nav>
     </header>
